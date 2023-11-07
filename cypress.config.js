49d92f4f0ea20e97b9944199122a15d29594a7d3
kristinap8 <kristinapoberezhna8@gmail.com>
2023-11-07 16:01:34 +0100

    first commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "cypress-multi-reporters",
 
-  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    configFile: "reporter-config.json",
+  },
 
   e2e: {
     setupNodeEvents(on, config) {
-
-      require('cypress-mochawesome-reporter/plugin')(on);
+      // implement node event listeners here
     },
   },
 });
